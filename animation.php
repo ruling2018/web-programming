c4b7f84e52dd3c9d7e8d7a50eecfd3a5836da5a5
ruling2018 <38260357+ruling2018@users.noreply.github.com>
2018-07-22 14:13:10 +0300

    Create animation.php

---
+++ animation.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Animation</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="/animation.css" />
+		<script tpye="text/javascirpt">
+		</script>
+	</head>
+	<body>
+		<!-- Preloader -->
+		<script type="text/javascript">
+    		//<![CDATA[
+        	/*$('html').load(function() {
+        		alert("Sayfa yüklendi (Çalışmadı kör olasıca)");
+        		$('#Container').fadeOut('slow');
+        		$('div.subContainer').fadeOut('slow');
+        		$('div.animation-1').fadeOut('slow');
+        		$('div.animation-2').fadeOut('slow');
+        		$('div.animation-3').fadeOut();
+        	})*/
+    		//]]>
+		</script>
+		<div id="Container" class="Container">
+			<div class="subContainer">
+				<div class="animation-1"></div>
+				<div class="animation-1"></div>
+				<div class="animation-1"></div>
+				<br /><br />
+				<div class="animation-2"></div>
+				<div class="animation-2"></div>
+				<div class="animation-2"></div>
+				<br /><br />
+				<div class="animation-3"></div>
+				<div class="animation-3"></div>
+				<div class="animation-3"></div>
+			</div>
+		</div>
+		<img src="" />
+	</body>
+</html>
